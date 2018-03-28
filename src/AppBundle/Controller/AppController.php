c60a6780a34960b4df5288f330fda2c1fdf6f5b9
Hinitial <oc.projet.super@gmail.com>
2018-03-28 12:06:29 +0200

    debut formulaire conttc close #9

--- src/AppBundle/Controller/AppController.php
+++ src/AppBundle/Controller/AppController.php
@@ -9,7 +9,13 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends Controller
@@ -25,8 +31,30 @@ class AppController extends Controller
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('@App/app/contact.html.twig');
+        $contact = new Contact();
+
+        $form = $this->createFormBuilder($contact)
+            ->add('nom',        TextType::class, array('required' => true))
+            ->add('prenom',     TextType::class, array('required' => true))
+            ->add('email',      EmailType::class, array('required' => true))
+            ->add('titre',      TextType::class, array('required' => true))
+            ->add('message',    TextareaType::class, array('required' => true))
+            ->add('envoyer',    SubmitType::class)
+            ->getForm()
+        ;
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $this->addFlash('notice', 'Formulaire bien envoyé');
+            }
+        }
+
+        return $this->render('@App/app/contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romain
+ * Date: 27/03/18
+ * Time: 13:38
+ */
+
+namespace AppBundle\Entity;
+
+
+class Contact
+{
+    private $nom;
+    private $prenom;
+    private $email;
+    private $titre;
+    private $message;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+
+}
